4edbeaafb2940d69a58900c6d6fd0e1ef100b836
Quentin <90897974+Quentiins@users.noreply.github.com>
2024-01-30 10:43:14 +0100

    Button

--- app/src/main/java/fr/solacroup/myproject/MainActivity.java
+++ app/src/main/java/fr/solacroup/myproject/MainActivity.java
@@ -6,16 +6,9 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView welcomeMessageTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.welcomeMessageTextView = findViewById(R.id.welcome_message);
-        this.welcomeMessageTextView.setText(
-                getResources().getText(R.string.hello_world) + "Solacroup"
-        );
     }
 }
